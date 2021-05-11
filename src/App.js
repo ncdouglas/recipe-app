b3abe0d7d182b9600510d08efd4edc2819c2dfda
Natasha Douglas <ncdouglas2016@gmail.com>
2021-05-11 06:54:29 -0500

    second

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = () => {
 
 const [recipes, setRecipes] = useState([]);
 const [search, setSearch] = useState('');
-const [query, setQuery] = useState('chicken');
+const [query, setQuery] = useState('favorite');
 
 useEffect(() => {
     getRecipes();
